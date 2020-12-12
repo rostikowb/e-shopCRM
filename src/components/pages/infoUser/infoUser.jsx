ea1f2ab0b84b60d85958285bcd3113cb3e4ada4a
micola10174 <micola.cascenco@gmail.com>
2020-12-12 11:37:00 +0200

    add button back to users

--- src/components/pages/infoUser/infoUser.jsx
+++ src/components/pages/infoUser/infoUser.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Formik, Form, Field} from 'formik';
-import {fetchAllUsers} from '../../../redux/users/actions';
+import { Formik, Form, Field } from 'formik';
+import { fetchAllUsers } from '../../../redux/users/actions';
+import { NavLink } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import DialogComponent from '../../HelperComponents/DialogComponent/DialogComponent';
 import RenderField from '../../HelperComponents/RenderField/RenderField';
@@ -19,7 +20,7 @@ class infoUser extends Component {
         const { match: { params }, token, fetchAllUsers } = this.props;
         fetchAllUsers(token, params.id);
     }
-    
+
 
     handleOpenModal = () => {
         this.setState({ isOpenModal: true })
@@ -32,12 +33,26 @@ class infoUser extends Component {
     render() {
         const { match: { params }, users } = this.props;
         const { isOpenModal } = this.state;
+        
         const user = users[0];
-        if(!user){
+        if (!user) {
             return <p>Loading....</p>
         }
         return (
             <div className="wrapper-info-user">
+                <NavLink to="/users" className="arrow">
+                    <svg width={20} height={20} viewBox="0 0 50 80">
+                        <path
+                            fill="none"
+                            stroke="#610c36"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="5"
+                            d="M45.63 75.8L.375 38.087 45.63.375"
+                        />
+                    </svg>
+                    <p>Users</p>
+                </NavLink>
                 <div className="container__info-user">
                     <h2>Id: {params.id}</h2>
                     <ul className="list__info-user">
@@ -84,7 +99,7 @@ class infoUser extends Component {
                     <div className="user-modal">
                         <h1>Изменение данных</h1>
                         <Formik
-                            initialValues={{ name: user.FN || '', last_name: '', patronymic: '', email: user.email || '', phone: '', city: '', department: '',  coupon: '', order: ''}}
+                            initialValues={{ name: user.FN || '', last_name: '', patronymic: '', email: user.email || '', phone: '', city: '', department: '', coupon: '', order: '' }}
                             onSubmit={(values, actions) => {
                                 setTimeout(() => {
                                     alert(JSON.stringify(values, null, 2));
@@ -92,19 +107,19 @@ class infoUser extends Component {
                                 }, 1000);
                             }}
                         >
-                                <Form>
-                                    <Field type="text" name="name" placeholder="Имя" component={RenderField} />
-                                    <Field type="text" name="last_name" placeholder="Фамилия" component={RenderField} />
-                                    <Field type="text" name="patronymic" placeholder="Отчество" component={RenderField} />
-                                    <Field type="email" name="email" placeholder="Email" component={RenderField} />
-                                    <Field type="text" name="phone" placeholder="Телефон" component={RenderField} />
-                                    <Field type="text" name="city" placeholder="Город" component={RenderField} />
-                                    <Field type="text" name="department" placeholder="Отделение" component={RenderField} />
-                                    <Field type="text" name="coupon" placeholder="Купоны" component={RenderField} />
-                                    <Field type="text" name="order" placeholder="Заказы" component={RenderField} />
-                                    {/* <Field name="lastName" placeholder="Doe" component={MyInput} /> */}
-                                    <button type="submit">Отправить</button>
-                                </Form>
+                            <Form>
+                                <Field type="text" name="name" placeholder="Имя" component={RenderField} />
+                                <Field type="text" name="last_name" placeholder="Фамилия" component={RenderField} />
+                                <Field type="text" name="patronymic" placeholder="Отчество" component={RenderField} />
+                                <Field type="email" name="email" placeholder="Email" component={RenderField} />
+                                <Field type="text" name="phone" placeholder="Телефон" component={RenderField} />
+                                <Field type="text" name="city" placeholder="Город" component={RenderField} />
+                                <Field type="text" name="department" placeholder="Отделение" component={RenderField} />
+                                <Field type="text" name="coupon" placeholder="Купоны" component={RenderField} />
+                                <Field type="text" name="order" placeholder="Заказы" component={RenderField} />
+                                {/* <Field name="lastName" placeholder="Doe" component={MyInput} /> */}
+                                <button type="submit">Отправить</button>
+                            </Form>
                         </Formik>
                     </div>
                 </DialogComponent>
@@ -114,7 +129,7 @@ class infoUser extends Component {
 }
 
 const mapStateToProps = state => {
-    return{
+    return {
         users: state.users.arr,
         token: state.auth.token,
     }
